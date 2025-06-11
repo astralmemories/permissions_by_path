66aed19ad5d2e6f344e19febb97b85286748a4ca
Leonardo Signorelli <astralmemories@gmail.com>
2025-06-12 01:23:16 +0200

    Multiple changes to the .module and NoticeController files, including the Warning and Error messages.

--- src/Controller/NoticeController.php
+++ src/Controller/NoticeController.php
@@ -1,169 +1,94 @@
 <?php
 
 namespace Drupal\permissions_by_path\Controller;
-use Drupal\Core\Controller\ControllerBase;
 
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\user\Entity\User;
+
+/**
+ * Returns responses for Permissions by Path routes.
+ */
 class NoticeController extends ControllerBase {
 
   /**
-   * returns a render-able array for a test page.
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
    */
+  protected $currentUser;
 
-  public function content() {
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
 
-    // Check who is visiting this page
-    $current_user = \Drupal::currentUser();
-    $user = \Drupal\user\Entity\User::load($current_user->id());
-    $username = $user->getAccountName();
-
-    // Get the configuration file
-    $config = \Drupal::config('permissions_by_path.settings');
-
-    // Create an empty array to store the paths that this current user has access to
-    $this_user_permissions = [];
-
-    // Grab the Permissions Paths with their List of users with access
-    $path1 = $config->get('path1');
-    $usernames_array1 = (array) $config->get('usernames1');
-
-    $path2 = $config->get('path2');
-    $usernames_array2 = (array) $config->get('usernames2');
-
-    $path3 = $config->get('path3');
-    $usernames_array3 = (array) $config->get('usernames3');
-
-    $path4 = $config->get('path4');
-    $usernames_array4 = (array) $config->get('usernames4');
-
-    $path5 = $config->get('path5');
-    $usernames_array5 = (array) $config->get('usernames5');
-
-    $path6 = $config->get('path6');
-    $usernames_array6 = (array) $config->get('usernames6');
-
-    $path7 = $config->get('path7');
-    $usernames_array7 = (array) $config->get('usernames7');
-
-    $path8 = $config->get('path8');
-    $usernames_array8 = (array) $config->get('usernames8');
-
-    $path9 = $config->get('path9');
-    $usernames_array9 = (array) $config->get('usernames9');
-
-    $path10 = $config->get('path10');
-    $usernames_array10 = (array) $config->get('usernames10');
-
-
-
-    // echo "<script>console.log('" . json_encode($usernames_array1) . "');</script>";
-
-    // Check if the current user exists on the usernames list 1
-    foreach ($usernames_array1 as &$value) {
-
-      if ($value == $username) {
-
-        array_push($this_user_permissions, $path1);
-      }
-    }
-
-    foreach ($usernames_array2 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path2);
-      }
-    }
-
-    foreach ($usernames_array3 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path3);
-      }
-    }
-
-    foreach ($usernames_array4 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path4);
-      }
-    }
-
-    foreach ($usernames_array5 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path5);
-      }
-    }
-
-    foreach ($usernames_array6 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path6);
-      }
-    }
-
-    foreach ($usernames_array7 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path7);
-      }
-    }
-
-    foreach ($usernames_array8 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path8);
-      }
-    }
-
-    foreach ($usernames_array9 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path9);
-      }
-    }
-
-    foreach ($usernames_array10 as &$value) {
-
-      if ($value == $username) {
-        array_push($this_user_permissions, $path10);
-      }
-    }
-
-
-
-    $list_of_paths = "";
-
-
-    // List the paths that this user can access in a single string variable
-    foreach ($this_user_permissions as &$permission_path) {
-
-      if ($list_of_paths == "") {
-        $list_of_paths = "&nbsp;&nbsp;&nbsp;&nbsp;&#8226; " . $permission_path;
-      }
-      else {
-        $list_of_paths = $list_of_paths . "<br>&nbsp;&nbsp;&nbsp;&nbsp;&#8226; " . $permission_path;
-      }
-
-    }
-
-
-//  class='container text-align-center'
-
-    return ['#markup' => "
-
-      <div class='container'>
-        <h3>Hello " . $username . "!<br> You don't have access to edit that page.</h3>
-        
-        <p>You have access to edit pages that contains the following path(s):</p>
-        
-        <p>" . $list_of_paths ."</p> 
-        
-        <p>Please contact web@uccs.edu is you have issues with this module.</p>
-      </div>
-
-    "];
-
-
-    return $build;
+  /**
+   * Constructs a NoticeController object.
+   */
+  public function __construct(AccountProxyInterface $current_user, ConfigFactoryInterface $config_factory) {
+    $this->currentUser = $current_user;
+    $this->configFactory = $config_factory;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Returns a render array for the test page.
+   */
+  public function content() {
+    $user = User::load($this->currentUser->id());
+    $username = $user ? $user->getAccountName() : '';
+
+    // Get the configuration.
+    $config = $this->configFactory->get('permissions_by_path.settings');
+    $module_enabled = $config->get('module_enable');
+    $affected_roles = $config->get('affected_roles') ?: [];
+    $affected_node_forms = $config->get('affected_node_forms') ?: [];
+    $path_permissions = $config->get('path_permissions') ?: [];
+
+    // Get the current user's roles.
+    $user_roles = $this->currentUser->getRoles();
+
+    // Check if the module is enabled and if the user has an affected role.
+    $is_affected = (bool) array_intersect($user_roles, $affected_roles);
+
+    // Find which paths this user has access to.
+    $user_paths = [];
+    foreach ($path_permissions as $mapping) {
+      if (!empty($mapping['path']) && !empty($mapping['users'])) {
+        if (in_array($username, $mapping['users'], TRUE)) {
+          $user_paths[] = $mapping['path'];
+        }
+      }
+    }
+
+    // Build a render array for demonstration.
+    return [
+      '#theme' => 'item_list',
+      '#title' => $this->t('Permissions by Path Debug Info'),
+      '#items' => [
+        $this->t('Module enabled: @enabled', ['@enabled' => $module_enabled ? 'Yes' : 'No']),
+        $this->t('Your username: @username', ['@username' => $username]),
+        $this->t('Your roles: @roles', ['@roles' => implode(', ', $user_roles)]),
+        $this->t('Affected roles: @roles', ['@roles' => implode(', ', $affected_roles)]),
+        $this->t('Affected content types: @types', ['@types' => implode(', ', $affected_node_forms)]),
+        $this->t('You have access to these paths: @paths', ['@paths' => $user_paths ? implode(', ', $user_paths) : $this->t('None')]),
+        $this->t('Are you affected by this module? @affected', ['@affected' => $is_affected ? 'Yes' : 'No']),
+      ],
+    ];
+  }
+
 }
